0800f1c34f241b0d5e9881ab1f49c1b5f63390fa
yaseenranta76@gmail.com <yaseenranta76@gmail.com>
2018-09-12 16:00:41 +0500

    convert data type into nullable.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,35 +1,48 @@
 ﻿using System;
 
-namespace HelloWorld
-{
     class Program
     {
         static void Main(string[] args)
         {
             // Print in console.
             Console.WriteLine("Application starts...");
-            //Console.WriteLine("Please Enter first name");
-            // Takes value from console
-            //String fname = Console.ReadLine();
-            //Console.WriteLine("Please Enter last name");
-            //String lname = Console.ReadLine();
-            //placeholder
-            //Console.WriteLine("Your name {0} {1}",fname ,lname);
-            //concatenation
-            //Console.WriteLine("Your name " + fname + " " + lname);
+        //Console.WriteLine("Please Enter first name");
+        // Takes value from console
+        //String fname = Console.ReadLine();
+        //Console.WriteLine("Please Enter last name");
+        //String lname = Console.ReadLine();
+        //placeholder
+        //Console.WriteLine("Your name {0} {1}",fname ,lname);
+        //concatenation
+        //Console.WriteLine("Your name " + fname + " " + lname);
 
-            // variable declaration
+        // variable declaration
 
-            //Boolean b = true;
-            //int i = 0;
+        //Boolean b = true;
+        //int i = 0;
 
-            //Console.WriteLine("Int Min value : " + int.MinValue + " Max value :" + int.MaxValue);
-            //Console.WriteLine("Short Min value : " + short.MinValue + " Max value :" + short.MaxValue);
+        //Console.WriteLine("Int Min value : " + int.MinValue + " Max value :" + int.MaxValue);
+        //Console.WriteLine("Short Min value : " + short.MinValue + " Max value :" + short.MaxValue);
 
-            //String s = "ONE\nTWO\nThree";
-            //String s = "it's a pencil.";
-            //Console.WriteLine(s);
+        //String s = "ONE\nTWO\nThree";
+        //String s = "it's a pencil.";
+        //Console.WriteLine(s);
+
+        // NULLABLE DATATYPE 
+        //(? is convert data type into nullable)
+        
+        int ?ticketOnSale = 0;
+            
+        if(ticketOnSale > 0)
+        {
+            Console.WriteLine("Available Ticket : {0}", ticketOnSale);
+        }else if(ticketOnSale <= 0 )
+        {
+            Console.WriteLine("Available Ticket : {0}",ticketOnSale);
+        }else if(ticketOnSale == null)
+        {
+            Console.WriteLine("Ticket is not Available");
+        }
 
         }
-    }
 }
