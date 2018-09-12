dc22ea4e17beae81540507e9bef9eb8f49fa2ee0
yaseenranta76@gmail.com <yaseenranta76@gmail.com>
2018-09-12 06:06:57 +0500

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print in console.
+            Console.WriteLine("Application starts...");
+            //Console.WriteLine("Please Enter first name");
+            // Takes value from console
+            //String fname = Console.ReadLine();
+            //Console.WriteLine("Please Enter last name");
+            //String lname = Console.ReadLine();
+            //placeholder
+            //Console.WriteLine("Your name {0} {1}",fname ,lname);
+            //concatenation
+            //Console.WriteLine("Your name " + fname + " " + lname);
+
+            // variable declaration
+
+            //Boolean b = true;
+            //int i = 0;
+
+            //Console.WriteLine("Int Min value : " + int.MinValue + " Max value :" + int.MaxValue);
+            //Console.WriteLine("Short Min value : " + short.MinValue + " Max value :" + short.MaxValue);
+
+            //String s = "ONE\nTWO\nThree";
+            //String s = "it's a pencil.";
+            //Console.WriteLine(s);
+
+        }
+    }
+}
