52914db351495018871cf1bf0d4d9f7229a6a3ae
yaseenranta76@gmail.com <yaseenranta76@gmail.com>
2018-09-14 12:27:13 +0500

    for, while, do while practice.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,40 +1,46 @@
 ﻿using System;
 
+
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
             Console.WriteLine("Application starts...");
-            Console.WriteLine("Enter Your Number");
-            int lmt = 0;
-            bool isNumber =   int.TryParse(Console.ReadLine(), out lmt);
-
-            if (isNumber)
-            {
-                Console.WriteLine("For loop started!");
-                for (int i = 0; i < lmt; i++)
-                {
-                    Console.WriteLine(i+'\n'); 
-                }
-                Console.WriteLine("Loop Ended!");
-                //Console.WriteLine("While loop started!");
-                //int start = 0;
-                //while (start <= lmt) {
-                //    Console.WriteLine(start + '\n');
-                //    start = start + 2;
-                //}
-                //Console.WriteLine("While loop Ended!");
-
-            }
-            else
-            {
-                Console.WriteLine("Please enter valid number");
-            }
-
             
+            String userChoice = "";
+            
+            do
+            {
+                Console.WriteLine("Enter Your Number");
+                int lmt = 0;
+                bool isNumber = int.TryParse(Console.ReadLine(), out lmt);
+
+                if (isNumber)
+                {
+                    Console.WriteLine("For loop started!");
+                    for (int i = 0; i < lmt; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Console.WriteLine("Loop Ended!");
+
+                }else{
+                    Console.WriteLine("Please enter valid number");
+                }
+
+                do
+                {
+                    Console.WriteLine("Do you want to continue again? Please choice Yes or No");
+                    userChoice = Console.ReadLine();
+                    if (userChoice.ToUpper() != "YES" && userChoice.ToUpper() != "NO") {
+                        Console.WriteLine("Invalid choice");
+                    }
+                    
+                }while (userChoice.ToUpper() != "YES" && userChoice.ToUpper() != "NO");
+                
+            } while (userChoice.ToUpper() == "YES");
         }
     }
 }
