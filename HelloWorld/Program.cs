10a323af6a629f08334e64ba7fbcf29b6cdffc56
yaseenranta76@gmail.com <yaseenranta76@gmail.com>
2018-09-14 19:12:00 +0500

    Method parameter types practice.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,13 +4,24 @@ namespace HelloWorld
 {
     class Program
     {
+        /*
+         there are 4 types of parameter we can pass in method
+         * Value parameter
+         * Reference parameter
+         * out parameter
+         * Array parameter 
+         * Defination - Value parameter does not affect on other parameter. its takes different memory location.
+         * Defination - Reference parameter affect on other parameter. its point on same memory location.
+         * Defination - out parameter gives one or more than one value.
+         * Defination - Array Parameter use multiple value of same data type.
+         */
         static void Main(string[] args)
         {
             // Print in console.
             Console.WriteLine("Application starts...");
             YrMethods yrM = new YrMethods();
             String str = yrM.str();
-            //string
+            //string and it is a example of value parameter
             Console.WriteLine(str);
             //sum
             int sum = YrMethods.add(4, 40);
@@ -18,6 +29,31 @@ namespace HelloWorld
 
             int minus = YrMethods.subtract(4, 40);
             Console.WriteLine("Subtract : {0}", minus);
+            //reference parameter
+            Console.WriteLine("-------------------Reference parameter---------------------------");
+            YrMethods.ref_param(ref sum);
+            Console.WriteLine("change in sum var : {0}", sum);
+            //out parameter
+            Console.WriteLine("----------------------Out parameter------------------------------");
+            int total = 0;
+            int product = 0;
+            YrMethods.out_param(10, 20, out total, out product);
+            Console.WriteLine("Total : {0} && Product : {1}", total,product);
+            Console.WriteLine("--------------------Array parameter------------------------------");
+            //Array Parameter
+            int cnt = 0;
+            int[] Numbers = new int[3];
+            Numbers[0] = 101;
+            Numbers[1] = 102;
+            Numbers[2] = 103;
+            YrMethods.array_param(out cnt, Numbers);
+            
+            Console.WriteLine("Total {0} Elements",cnt);
+            
+            Console.WriteLine("------------Array parameter without passing Array----------------");
+            YrMethods.array_param(out cnt);
+
+            Console.WriteLine("Total {0} Elements", cnt);
         }
     }
 }
--- HelloWorld/YrMethods.cs
+++ HelloWorld/YrMethods.cs
@@ -4,16 +4,16 @@ namespace HelloWorld
 {
     class YrMethods
     {
-        
+
         public string str() {
 
             String str = "This is String return method.";
             return str;
         }
 
-        public static int add (int a = 0, int b = 0) {
+        public static int add(int a = 0, int b = 0) {
 
-            int sum = a + b;     
+            int sum = a + b;
             return sum;
         }
 
@@ -21,6 +21,29 @@ namespace HelloWorld
 
             int minus = a - b;
             return minus;
+        }
+
+        //used as ref parameter example
+
+        public static void ref_param(ref int param){
+            param = 100;
+        }
+
+        public static void out_param(int a, int b, out int total, out int product) {
+            total   = a + b;
+            product = a * b;
+                
+        }
+        /*
+         * makes array parameter optional use params keyword
+         * params keyword only once use in method
+         * params keyword must be last in parameter
+         */
+        public static void array_param(out int cnt, params int [] Numbers) {
+            cnt = Numbers.Length;
+            foreach (int nm in Numbers) {
+                Console.WriteLine(nm);
+            }
         }
     }
 }
