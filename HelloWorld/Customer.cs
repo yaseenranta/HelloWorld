0f9a10fdac314c0bf27b43ebe0efbe961a5d8a15
yaseenranta76@gmail.com <yaseenranta76@gmail.com>
2018-09-22 13:50:07 +0500

    interface declaration and implementation, interface explicit implementaton practice.

---
+++ HelloWorld/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    //Interface can not be initial
+    //A class can inherit one or more interface
+    //Interface don't have fields and property
+    //Interface have only declaration,no implementation
+    //Interface does not offer construct.
+    interface ICustomer
+    {        
+        void print();                
+    }
+
+    interface IStudent
+    {
+        void printDetail();
+        String studentList();    
+
+    }
+
+    interface IStudentResult
+    {
+        void printDetail();
+    }
+    
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,6 +8,44 @@ namespace HelloWorld
         {
             // Print in console.
             Console.WriteLine("Application starts...");
+            Student Student = new Student();
+            IStudentResult StudentResult = new Student();
+            Student.Id = 1;
+            Student.Email = "yaseenranta76@gmail.com";
+            Student.Fullname = "Muhammad yaseen";
+            Student.Classname = 9;
+            //explicit interface implementation.
+            ((IStudent)Student).printDetail();
+
+            StudentResult.printDetail();
+
+
+        }
+    }
+
+    class Student : IStudent, IStudentResult
+    {
+        public int Id { set; get; }
+        public string Email { set; get; }
+        public string Fullname { set; get; }
+        public int Classname { set; get; }
+
+        public  void  printDetail()
+        {
+            Console.WriteLine("Student Id    : {0}",this.Id);
+            Console.WriteLine("Student Name  : {0}", this.Fullname);
+            Console.WriteLine("Student Email : {0}",this.Email);
+            Console.WriteLine("Student Class : {0}",this.Classname);
+        }
+
+        void IStudentResult.printDetail()
+        {
+            Console.WriteLine("Student Result Detail");            
+        }
+
+        public String studentList()
+        {
+            return "Student List method";
         }
     }
 }
