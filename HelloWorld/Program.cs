2bd3a955212f9692a9c69398165bf47177387b1c
yaseenranta76@gmail.com <yaseenranta76@gmail.com>
2018-09-27 10:31:55 +0500

    Reflection practice

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Reflection;
 namespace HelloWorld
 {
     class Program
@@ -8,6 +9,82 @@ namespace HelloWorld
         {
             // Print in console.
             Console.WriteLine("Application starts...");
+
+            Office.employees.Add(new Employee { Name = "Muhammad Yaseen", Salary = 5000, Age = 26 });
+            Office.employees.Add(new Employee { Name = "Muhammad Altaf", Salary = 5000, Age = 26 });
+            //List<Employee> Employees = Office.getEmployee();
+
+            Assembly assemblyExecute = Assembly.GetExecutingAssembly();
+            Type office = assemblyExecute.GetType("HelloWorld.Office");
+            object officeInstance = Activator.CreateInstance(office);
+            MethodInfo methodInfo = office.GetMethod("getEmployee");
+            ParameterInfo[] parameterInfos = methodInfo.GetParameters();            
+            
+            List<Employee> Employees = (List<Employee>)methodInfo.Invoke(officeInstance, parameterInfos);
+
+            foreach (Employee employee in Employees)
+            {
+                Console.WriteLine("Employee name is {0} and monthly salary is {1} and age is {2}",employee.Name,employee.Salary,employee.Age);
+            }
         }
+
     }
+    #region Office class
+    class Office
+    {
+        #region Fields
+        public static List<Employee> employees = new List<Employee>();
+
+        #endregion
+
+        #region Methods
+        
+        public static List<Employee> getEmployee()
+        {
+            
+            return employees;
+        }
+        #endregion
+
+    }
+    #endregion
+
+    #region Employee Class
+    class Employee
+    {
+        #region Fields
+        private static string _name;
+        private static double _salary;
+        private static int _age;
+        #endregion
+
+        #region Properties
+        public String Name {
+            set { _name = value; }
+            get {return _name; }
+        }
+        public double Salary { set { _salary = value; } get { return _salary; } }
+        public int Age { set { _age = value; } get { return _age; } }
+        #endregion
+
+        #region Methods
+        public double getSalary()
+        {
+            return _salary;
+        }
+
+        public int getAge()
+        {
+            return _age;
+        }
+
+        public void getDetail()
+        {
+
+        }
+        #endregion
+    }
+    #endregion
+
+
 }
